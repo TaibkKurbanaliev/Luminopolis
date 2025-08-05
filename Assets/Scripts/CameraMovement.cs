907f35ae31e4ff5f2870d07de0d0819710672572
TaibkKurbanaliev <taibkurbanaliev@gmail.com>
2025-08-05 22:47:04 +0400

    Add wasd movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using Zenject;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Transform _cameraTarget;
+    [SerializeField] private float _moveSpeed;
+
+    private InputSystem_Actions _input;
+
+    [Inject]
+    private void Constract(InputSystem_Actions input)
     {
-        
+        _input = input;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        Move();
     }
+
+    private void Move()
+    {
+        var cameraFwd = Camera.main.transform.forward;
+        cameraFwd.y = 0;
+        cameraFwd.Normalize();
+        
+        var cameraRight = Camera.main.transform.right;
+        cameraRight.y = 0;
+        cameraRight.Normalize();
+
+        var playerInput = _input.Player.Move.ReadValue<Vector2>();
+        var targetSpeed = new Vector3(playerInput.x, 0f, playerInput.y) * _moveSpeed * Time.fixedDeltaTime;
+        
+        var targetDir =  cameraFwd * targetSpeed.z + cameraRight * targetSpeed.x;
+
+        _cameraTarget.position += targetDir * Time.fixedDeltaTime;
+    }    
 }
